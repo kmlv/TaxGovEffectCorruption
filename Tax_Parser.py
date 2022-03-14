fd7b5ac8aa257a0ecf3d2418c16a5de4ce45aeb5
Sergio2405 <67673799+Sergio2405@users.noreply.github.com>
2022-03-14 12:01:43 -0500

    parse updated: group_id

--- Tax_Parser.py
+++ Tax_Parser.py
@@ -1,8 +1,8 @@
 import pandas as pd 
 
-data = pd.read_csv("data_acum16.csv", header = [0,1]) # data acumulada
+data = pd.read_csv("session2_parsed.csv") # data acumulada
 
-new_session = pd.read_csv(r"C:\Users\Sergio\Downloads\sesion4.csv") # new session data
+new_session = pd.read_csv(r"C:\Users\Sergio\Downloads\sesion3.csv") # new session data
 
 #### Parser ####
 
@@ -13,32 +13,16 @@ ids = new_session[
     ]
 ] 
 
-columns_names = [
-    'Code',
-    'Label',
-]
-
-header = ['Participant']*2
-ids.columns = [header,columns_names]
-
 dictator = new_session[
     [
         'dictator.1.player.dictator',
+        'dictator.1.player.id_in_group',
+        'dictator.1.group.id_in_subsession',
         'dictator.1.player.kept',
         'dictator.1.group.group_kept',
         'dictator.1.player.payoff'
     ]
 ] 
-
-columns_names = [
-    'Dictador',
-    'Decisión',
-    'Resultado',
-    'Payoff_Dictator'
-]
-
-header = ['Dictator']*4
-dictator.columns = [header,columns_names] 
 
 # trust
 sent_back_strat = ["trust.1.player.sent_back_amount_strategy_"+str(i)
@@ -48,45 +32,27 @@ variables = [
         'trust.1.player.trustor',
         'trust.1.player.sent_amount_strategy',
         'trust.1.player.trustee',
+        'trust.1.player.id_in_group',
+        'trust.1.group.id_in_subsession',
     ] + sent_back_strat +  [
         'trust.1.group.sent_amount',
         'trust.1.group.sent_back_amount',
         'trust.1.player.payoff'
     ]
+
 trust = new_session[variables] 
-
-labels = {
-    'trust.1.player.trustor':'trustor',
-        'trust.1.player.sent_amount_strategy':'sent_amount_strategy',
-        'trust.1.player.trustee':'trustee',
-    'trust.1.group.sent_amount':'sent_amount',
-        'trust.1.group.sent_back_amount':'sent_back_amount',
-        'trust.1.player.payoff':'Payoff_Trust'
-}
-
-header = ['Trust']*17
-trust.columns = [header,variables]
-trust = trust.rename(columns = labels) 
 
 # public goods
 public_goods = new_session[
     [
         'public_goods.1.player.contribution',
+        'public_goods.1.player.id_in_group',
+        'public_goods.1.group.id_in_subsession',
         'public_goods.1.group.total_contribution',
         'public_goods.1.group.individual_share',
         'public_goods.1.player.payoff'
     ]
-] 
-
-columns_names = [
-    'Contribution',
-    'Total Contribution',
-    'Individual Share',
-    'Payoff_Public_Goods'
-]
-
-header = ['Public_Goods']*4
-public_goods.columns = [header,columns_names]  
+]  
 
 # coin tossing
 variables = []
@@ -108,8 +74,7 @@ coin_tossing['Payoff_Coin_Tossing'] = sum([coin_tossing['coin_tossing.'+str(i)+'
 variables = ['Num Lies','% Lies','Payoff_Coin_Tossing']
 coin_tossing = coin_tossing[variables]
 
-header = ['Coin_Tossing']*3
-coin_tossing.columns = [header,variables]
+coin_tossing.columns = [variables]
 
 final_data = pd.concat([ids,dictator,trust,public_goods,coin_tossing], axis=1)
 
@@ -117,5 +82,5 @@ acum_data = pd.concat([data,final_data]) # concat all data sessions with the new
 
 #### End Parser ####
 
-acum_data.to_csv("data_updated.csv", mode="a",index=False) #exporting the new data
+acum_data.to_csv("session3_parsed.csv") #exporting the new data
 
